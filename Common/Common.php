addd8424505b50251c7e89514c69e08ecbaf4113
lvruizheng <15810312145@163.com>
2017-07-21 10:15:46 +0800

    修改

--- Common/Common.php
+++ Common/Common.php
@@ -18,34 +18,4 @@ class Common{
     static public $auth_secret = 'dzkcw_auth';
     
     
-    /**
-     * 发送get请求
-     * @return array
-     */
-    static public function sendHttp($url){
-        $client = new Client();
-        $request = $client->get($url);
-    
-        return json_decode($request->getBody(),true);
-    }
-    
-    /**
-     * md5方式签名
-     * @param  array $params 待签名参数
-     * @return string
-     */
-    static public function generateMd5Sign($params)
-    {
-        ksort($params);
-    
-        $tmps = array();
-        foreach ($params as $k => $v) {
-            $tmps[] = $k . $v;
-        }
-    
-        $string = self::$app_secret . implode('', $tmps) . self::$app_secret;
-    
-        return strtoupper(md5($string));
-    }
-    
 }
